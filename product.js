62dd2f004a3d1fc9f2da97c8f498244e1d9f7f13
gabel-jacob <gabel056@umn.edu>
2026-03-23 13:06:54 -0500

    Create products object with item details
    Added a products object containing details for six items.

---
+++ product.js
@@ -0,0 +1,42 @@
+const products = {
+  item1: {
+    name: "Item 1",
+    price: 25,
+    image: "pictures/DRWBYtv.jpeg",
+    description: "Minimal design piece."
+  },
+
+  item2: {
+    name: "Item 2",
+    price: 10,
+    image: "pictures/FqSCOvL.jpeg",
+    description: "Retro inspired product."
+  },
+  item3: {
+    name: "Item 3",
+    price: 25,
+    image: "pictures/QLnZAoA.jpeg",
+    description: "Minimal design piece."
+  },
+
+  item4: {
+    name: "Item 4",
+    price: 20,
+    image: "pictures/VNhvvaN.jpeg",
+    description: "Retro inspired product."
+  },
+  item5: {
+    name: "Item 5",
+    price: 25,
+    image: "pictures/p995ibU.jpeg",
+    description: "Minimal design piece."
+  },
+
+  item6: {
+    name: "Item 6",
+    price: 20,
+    image: "pictures/zBYUKh5.jpeg",
+    description: "Retro inspired product."
+  }
+
+};
